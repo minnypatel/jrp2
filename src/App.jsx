df1579c5c83a401df1a5606ffb0f5ef708e6aafe
minnypatel <minnypatel@googlemail.com>
2018-11-02 12:47:06 +0000

    add basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Child } from './Child';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Home, About, Topics } from './Child';
 
 class App extends React.Component {
   componentDidMount() {
     // const promise = petfinder.breed.list({ animal: 'dog' });
     // promise.then(console.log(), console.error);
+    console.log('hioadhfoiasdfh');
   }
 
   render() {
     return (
       <div>
         <h1>App.js</h1>
-        <Child message="passed from app" />
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">HII</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/topics">Topics</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+          </div>
+        </Router>
       </div>
     );
   }
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,9 +1,93 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export const Child = props => {
+// const BasicExample = ({ message }) => {
+//   BasicExample.propTypes = {
+//     message: PropTypes.string.isRequired,
+//   };
+
+//   console.log(message);
+
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">{message}</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/topics">Topics</Link>
+//           </li>
+//         </ul>
+
+//         <hr />
+
+//         <Route exact path="/" component={Home} />
+//         <Route path="/about" component={About} />
+//         <Route path="/topics" component={Topics} />
+//       </div>
+//     </Router>
+//   );
+// };
+
+function Topic({ match }) {
   return (
     <div>
-      <h1>{props.message}</h1>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
+}
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 };
+
+export const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
+
+export const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+// export default BasicExample;
+
+// export const Child = props => {
+//   return (
+//     <div>
+//       <h1>{props.message}</h1>
+//     </div>
+//   );
+// };
