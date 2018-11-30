ef356ffcaebe9ceb1fec4918c59a2a4a37f2cd51
minnypatel <minnypatel@googlemail.com>
2018-11-30 13:06:55 +0000

    add component

--- src/Child.jsx
+++ src/Child.jsx
@@ -2,38 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-// const BasicExample = ({ message }) => {
-//   BasicExample.propTypes = {
-//     message: PropTypes.string.isRequired,
-//   };
-
-//   console.log(message);
-
-//   return (
-//     <Router>
-//       <div>
-//         <ul>
-//           <li>
-//             <Link to="/">{message}</Link>
-//           </li>
-//           <li>
-//             <Link to="/about">About</Link>
-//           </li>
-//           <li>
-//             <Link to="/topics">Topics</Link>
-//           </li>
-//         </ul>
-
-//         <hr />
-
-//         <Route exact path="/" component={Home} />
-//         <Route path="/about" component={About} />
-//         <Route path="/topics" component={Topics} />
-//       </div>
-//     </Router>
-//   );
-// };
-
 function Topic({ match }) {
   return (
     <div>
